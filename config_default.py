5fd3b916d91ac59e950f3c5158444ce1e0ff8533
zhbnglmm <zhbnglbn@163.com>
2020-09-08 09:20:12 +0800

    init

---
+++ config_default.py
@@ -0,0 +1,15 @@
+# encoding : utf-8
+import os
+
+DEBUG = True
+USERNAME = "root"
+PASSWORD = "root"
+HOST = "127.0.0.1"
+DATABASE = "learnnote3"
+SECRET_KEY = os.urandom(24)
+LOG_FILE_FULL_PATH = "C:\\Alan\\learnnote3\\learnnote_server\\flasklog.log"
+DIALECT = "mysql"
+DRIVER = "pymysql"
+PORT = "3306"
+SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ learnnote3.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+import sys
+import os
+try:
+    import config_override as config
+except ImportError:
+    import config_default as config
+from exts import db
+import logging
+import json
+app = Flask(__name__)
+app.config.from_object(config)
+db.init_app(app)
+
+
+
+
+app.debug = True
+handler = logging.FileHandler(config.LOG_FILE_FULL_PATH, encoding='UTF-8')
+handler.setLevel(logging.DEBUG)
+logging_format = logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')
+handler.setFormatter(logging_format)
+app.logger.addHandler(handler)
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
+    pass
